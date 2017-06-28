dc146f47dd8f40ad176dbe629d48d095ac1505d1
Macbook <macbook@Macbooks-MacBook-Pro-4.local>
2017-06-27 19:44:12 -0500

    Add peso outlet and text field.

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -5,10 +5,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var poundLabel: UILabel!
     @IBOutlet weak var yenLabel: UILabel!
     @IBOutlet weak var euroLabel: UILabel!
+    @IBOutlet weak var pesoLabel: UILabel!
     @IBOutlet weak var inputTextField: UITextField!
     let poundRate = 0.69
     let yenRate = 113.94
     let euroRate = 0.89
+    let pesoRate = 0.056
     var dollarAmount = 0.0
     
     @IBAction func clearButton(_ sender: UIButton) {
@@ -16,11 +18,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
         poundLabel.text = "0.00"
         yenLabel.text = "0.00"
         euroLabel.text = "0.00"
+        pesoLabel.text = "0.00"
+        
     }
     @IBAction func convertCurrency(_ sender: UIButton) {
         if let amount = Double(inputTextField.text!) {
             dollarAmount = amount
         }
+        pesoLabel.text = "\(dollarAmount * pesoRate)"
         poundLabel.text = "\(dollarAmount * poundRate)"
         yenLabel.text = "\(dollarAmount * yenRate)"
         euroLabel.text = "\(dollarAmount * euroRate)"
